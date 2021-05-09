e2801a73073299fbad2497efc06194193abb7c8d
Andrew Aleynikov <drew.lake@yandex.ru>
2021-05-09 23:08:11 +0300

    добавил перманентный бан тем, кто не состоит в группе, но зашел в чат

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import re
 
 import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.longpoll import VkLongPoll, VkEventType, VkChatEventType
 
 from config import token, group_id
 
@@ -24,14 +24,23 @@ def get_name(uid: int) -> str:
     return "{}".format(data["first_name"])
 
 
-def is_group_admin(group_id, message_owner_id):
-    managers = vk_session.get_api().groups.getMembers(group_id=group_id, filter='managers').get('items')
+def is_group_admin(of_group_id, message_owner_id):
+    managers = vk_session.get_api().groups.getMembers(group_id=of_group_id, filter='managers').get('items')
     for manager in managers:
         manager_id = manager.get('id')
         manager_role = manager.get('role')
         if manager_id == message_owner_id and manager_role in super_user_group_roles:
             return 1
     return 0
+
+
+def remove_user_from_chat(from_chat_id, to_user_id):
+    vk_session.get_api().messages.removeChatUser(chat_id=from_chat_id, user_id=to_user_id)
+
+
+def is_group_member(of_group_id, user_id_to_check):
+    is_member = vk_session.get_api().groups.isMember(group_id=of_group_id, user_id=user_id_to_check)
+    return is_member
 
 
 def parse_words(text):
@@ -44,6 +53,13 @@ def delete_message(event, message_reply):
 
 
 for event in long_poll.listen():
+    if event.type == VkEventType.CHAT_UPDATE:
+        if event.update_type == VkChatEventType.USER_JOINED:
+            joined_user_id = event.info.get('user_id')
+            if not is_group_admin(group_id, joined_user_id):
+                if not is_group_member(group_id, joined_user_id):
+                    remove_user_from_chat(event.chat_id, joined_user_id)
+
     if event.type == VkEventType.MESSAGE_NEW:
         if event.from_chat:
             if not is_group_admin(group_id, event.user_id):
